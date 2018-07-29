1c6ad81fe6df5f388f37ae5b9405c1ed6e557e04
Richard <richardliuwa@gmail.com>
2018-07-28 22:28:03 -0700

    Created Post class

--- model.py
+++ model.py
@@ -4,3 +4,14 @@ class User:
     first_name = ndb.StringProperty(required=True)
     last_name = ndb.StringProperty(required=True)
     dp_url = ndb.StringProperty(required=True)
+    id = ndb.IntegerProperty(required=True)
+
+class Post:
+    author_id = ndb.IntegerProperty(required=True)
+    author_key = ndb.KeyProperty(User)
+    text = ndb.StringProperty(required=True)
+    img_url = ndb.StringProperty(required=True)
+    timestamp = ndb.DateTimeProperty(required=True)
+    score = ndb.IntegerProperty(required=True)
+    comments = ndb.StringProperty(repeated=True)
+    
