86583f70c39c3b5c2ffd46715805c7101464b7c2
Richard <richardliuwa@gmail.com>
2018-07-30 10:37:47 -0700

    Added urllib

--- main.py
+++ main.py
@@ -14,6 +14,8 @@
 
 import webapp2
 import jinja2
+import urllib
+import urllib2
 import os
 
 
@@ -32,6 +34,8 @@ class WelcomePage(webapp2.RequestHandler):
     def get(self):
         results_template = JINJA_ENVIRONMENT.get_template('templates/welcome.html')
         self.response.write(results_template.render())
+    def index(self):
+
 
 class ProfilePage(webapp2.RequestHandler):
     def get(self):
